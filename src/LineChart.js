d20d5afa677963f6fda55f5bfb3ad26108024812
diannediamante <mddiamante@up.edu.ph>
2021-07-15 18:15:47 +0800

    Initalize d3

---
+++ src/LineChart.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useRef} from 'react';
+
+import * as d3 from 'd3';
+import './LineChart.css';
+
+const LineChart = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default LineChart;
